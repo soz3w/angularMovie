db42572d557d124492e54167b2ea0acb7c16322f
soz3w <saadnaz@yahoo.fr>
2015-06-01 11:30:25 +0200

    first commit

---
+++ app/js/controllers.js
@@ -0,0 +1,77 @@
+"use strict";
+
+angularMovieApp.controller("homeController" ,function ($scope) {
+
+    $scope.user = 'Saadna';
+
+});
+
+angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
+
+    // display mode by default
+    $scope.tableView = false;
+    // icon by mode by default
+    $scope.tableViewIcon = 'icon-th-list icon-white';
+
+    // function called when changing view mode
+    $scope.toogleView = function() {
+        $scope.tableView = !$scope.tableView;
+
+        if($scope.tableView === false){
+            $scope.tableViewIcon = 'icon-th-list icon-white';
+        } else {
+            $scope.tableViewIcon = 'icon-th icon-white';
+        }
+    };
+
+    Movie.fetch().success(function(resp){
+        $scope.movies = resp;
+    });
+
+    $scope.deleteMovie = function(movie){
+		var index = $scope.movies.indexOf(movie);
+
+        Movie.remove(movie.id)
+            .success(function(){
+                $scope.movies.splice(index, 1);
+            }
+        );
+    };
+
+});
+
+
+angularMovieApp.controller('editMovieController', function($scope, Movie, $routeParams, $location){
+
+    var movieId = $routeParams.id;
+
+    Movie.fetchOne(movieId).success(function(movie){
+       $scope.movie = movie;
+    });
+
+    $scope.updateMovie = function(movie){
+       Movie.update(movie)
+           .success(function(){
+               $location.path('/movies');
+           })
+           .error(function(resp){
+               console.log(resp);
+           });
+    };
+});
+
+angularMovieApp.controller("movieFormController" ,function ($scope, Movie) {
+
+    $scope.addMovie = function(movie){
+
+        Movie.create(movie)
+            .success(function(){
+                $scope.movies.push(movie);
+                $scope.movie = {};
+            })
+            .error(function(resp){
+                console.log(resp);
+            });
+
+    };
+});
---
+++ readme.txt
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=C7ZI7z7qnHU
+
+Firebase : a realtime synchronization network
+data is represented by a url
+firbase.js
+angularFire.js
+json web token
